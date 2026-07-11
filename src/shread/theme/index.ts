f60283176179b2e83cfd9289e9a3541a651ac63c
keslergokhan <58035013+keslergokhan@users.noreply.github.com>
2026-07-11 23:33:25 +0300

    theme fix

--- src/shread/theme/index.ts
+++ src/shread/theme/index.ts
@@ -1,6 +1,7 @@
 import {color} from "@/shread/theme/color"
 import {typography} from "@/shread/theme/typography"
 import {spacing} from "@/shread/theme/spacing";
+import {radius_} from "@/shread/theme/radius"
 
 
 const theme = {
@@ -13,5 +14,6 @@ type theme = typeof theme;
 type colors = typeof color;
 type typographys = typeof typography;
 type spacings = typeof spacing;
+type radius = typeof radius_;
 export default theme;
-export {colors,typographys,spacings}
+export {colors,typographys,spacings,radius}
---
+++ src/shread/theme/radius.ts
@@ -0,0 +1,18 @@
+export const radius_ = {
+
+
+    xs: 2,
+
+    sm: 4,
+
+    md: 8,
+  
+    lg: 12,
+  
+    xl: 16,
+  
+    xxl: 32,
+ 
+    xxxl: 64,
+  
+  } as const;
